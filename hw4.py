40edd990cc02de0d2c352c0fc08ca488782a2334
Lucas Tindall <ltindall@ucsd.edu>
2017-05-31 23:31:35 -0700

    Added final py code and loss txt files

--- hw4.py
+++ hw4.py
@@ -1,118 +1,92 @@
 
 # coding: utf-8
 
-# In[ ]:
+# # COGS 260 
+# # Assignment 4
+# 
+# ## Author: Lucas Tindall
 
-"""
-Minimal character-level Vanilla RNN model. Written by Andrej Karpathy (@karpathy)
-BSD License
-"""
-import numpy as np
+# ### Tinyshakespeare dataset
 
-# data I/O
-data = open('tinyshakespeare.txt', 'r').read() # should be simple plain text file
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print 'data has %d characters, %d unique.' % (data_size, vocab_size)
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
+# In[58]:
 
-# hyperparameters
-hidden_size = 200 # size of hidden layer of neurons
-seq_length = 25 # number of steps to unroll the RNN for
-learning_rate = 1e-1
 
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-bh = np.zeros((hidden_size, 1)) # hidden bias
-by = np.zeros((vocab_size, 1)) # output bias
+import matplotlib.pyplot as plt
 
-def lossFun(inputs, targets, hprev):
-  """
-  inputs,targets are both list of integers.
-  hprev is Hx1 array of initial hidden state
-  returns the loss, gradients on model parameters, and last hidden state
-  """
-  xs, hs, ys, ps = {}, {}, {}, {}
-  hs[-1] = np.copy(hprev)
-  loss = 0
-  # forward pass
-  for t in xrange(len(inputs)):
-    xs[t] = np.zeros((vocab_size,1)) # encode in 1-of-k representation
-    xs[t][inputs[t]] = 1
-    hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-    ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
-    ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
-    loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
-  # backward pass: compute gradients going backwards
-  dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-  dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-  dhnext = np.zeros_like(hs[0])
-  for t in reversed(xrange(len(inputs))):
-    dy = np.copy(ps[t])
-    dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
-    dWhy += np.dot(dy, hs[t].T)
-    dby += dy
-    dh = np.dot(Why.T, dy) + dhnext # backprop into h
-    dhraw = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
-    dbh += dhraw
-    dWxh += np.dot(dhraw, xs[t].T)
-    dWhh += np.dot(dhraw, hs[t-1].T)
-    dhnext = np.dot(Whh.T, dhraw)
-  for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
-  return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
+files = ['shakespeare_1layer.txt','tinyshakespeare_rnn_output.txt','tinyshakespeare_console.txt','shakespeare_100seq.txt','shakespeare_rnn160.txt']
 
-def sample(h, seed_ix, n):
-  """ 
-  sample a sequence of integers from the model 
-  h is memory state, seed_ix is seed letter for first time step
-  """
-  x = np.zeros((vocab_size, 1))
-  x[seed_ix] = 1
-  ixes = []
-  for t in xrange(n):
-    h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
-    y = np.dot(Why, h) + by
-    p = np.exp(y) / np.sum(np.exp(y))
-    ix = np.random.choice(range(vocab_size), p=p.ravel())
-    x = np.zeros((vocab_size, 1))
-    x[ix] = 1
-    ixes.append(ix)
-  return ixes
+x_all = []
+y_all = []
+for name in files: 
+    with open(name) as f:
+        mylist = f.read().splitlines() 
+        x = []
+        y = []
+        for a in mylist: 
+            l = a.split(' ')
 
-n, p = 0, 0
-mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
-while True:
-  # prepare inputs (we're sweeping from left to right in steps seq_length long)
-  if p+seq_length+1 >= len(data) or n == 0: 
-    hprev = np.zeros((hidden_size,1)) # reset RNN memory
-    p = 0 # go from start of data
-  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+            if len(l) == 12: 
+                x.append(l[6])
+                y.append(l[11])
+        x_all.append(x)
+        y_all.append(y)
 
-  # sample from the model now and then
-  if n % 100 == 0:
-    sample_ix = sample(hprev, inputs[0], 200)
-    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    print '----\n %s \n----' % (txt, )
 
-  # forward seq_length characters through the net and fetch gradient
-  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-  smooth_loss = smooth_loss * 0.999 + loss * 0.001
-  if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss) # print progress
-  
-  # perform parameter update with Adagrad
-  for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], 
-                                [dWxh, dWhh, dWhy, dbh, dby], 
-                                [mWxh, mWhh, mWhy, mbh, mby]):
-    mem += dparam * dparam
-    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+plt.figure(figsize=(10,7))
+plt.plot(y_all[4],'m', label='2 layers, seq_len=50, hidden=160, LSTM')
+plt.plot(y_all[0],'b', label='1 layer, seq_len=50, hidden=128, LSTM')
+plt.plot(y_all[1],'r', label='2 layers, seq_len=50, hidden=128, RNN')
+plt.plot(y_all[2],'g', label='2 layers, seq_len=50, hidden=128, LSTM')
+plt.plot(y_all[3],'y', label='2 layers, seq_len=100, hidden=128, LSTM')
 
-  p += seq_length # move data pointer
-  n += 1 # iteration counter 
+plt.legend(loc='upper center', shadow=True)
+plt.title('RNN trained on tinyshakespeare data')
+plt.ylabel('loss')
+plt.xlabel('iterations')
+plt.ylim([1,3])
+plt.show()
+
+
+
+
+
+
+# ### Nottingham Music Database
+
+# In[63]:
+
+files = ['music_1layer.txt','music_rnn_output.txt','music_regular.txt','music_100seq_output.txt','music_rnn192.txt']
+
+
+x_all = []
+y_all = []
+for name in files: 
+    with open(name) as f:
+        mylist = f.read().splitlines() 
+        x = []
+        y = []
+        for a in mylist: 
+            l = a.split(' ')
+            if len(l) == 12: 
+                x.append(l[6])
+                y.append(l[11])
+        x_all.append(x)
+        y_all.append(y)
+
+plt.figure(figsize=(10,7))
+plt.plot(y_all[4],'m', label='2 layers, seq_len=50, hidden=192, LSTM')
+plt.plot(y_all[0],'b', label='1 layer, seq_len=50, hidden=128, LSTM')
+plt.plot(y_all[1],'r', label='2 layers, seq_len=50, hidden=128, RNN')
+plt.plot(y_all[2],'g', label='2 layers, seq_len=50, hidden=128, LSTM')
+plt.plot(y_all[3],'y', label='2 layers, seq_len=100, hidden=128, LSTM')
+plt.legend(loc='upper center', shadow=True)
+plt.title('RNN trained on Nottingham Music Database')
+plt.ylabel('loss')
+plt.xlabel('iterations')
+plt.ylim([0,3])
+plt.show()
+
+
+
+
 
